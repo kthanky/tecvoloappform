04a790cca8e62d888c0e76dc9fced2aa26ac03c4
kthanky <kthanky@gmail.com>
2016-06-03 15:08:52 +0530

    storecsv.php files updated as per requirements

--- storecsv.php
+++ storecsv.php
@@ -5,16 +5,22 @@
  * Date: 6/3/2016
  * Time: 2:45 PM
  */
-if(isset($_POST['fn']))
+if(isset($_POST['submit']))
 {
     $csv=array();// take an array
+    $title=$_POST['title'];
     $fn = $_POST['firstname'];
     $ln = $_POST['lastname'];
-    $ln = $_POST['lastname'];
-    $ln = $_POST['lastname'];
-    $ln = $_POST['lastname'];
-    $ln = $_POST['lastname'];
-    $ln = $_POST['lastname'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'];
+    $address1 = $_POST['address1'];
+    $address2 = $_POST['address2'];
+    $nation = $_POST['nation'];
+    $edufield=$_POST['edufield'];
+    $edulevel=$_POST['edulevel'];
+    $gender=$_POST['gender'];
+    $precon=$_POST['precon'];
+    $dob=$_POST['dob'];
 
     $csv[]=$fn;
     $csv[]=$ln;  //push the values in an array.
